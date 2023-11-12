97ded9eeb2a411a5d2636e7a1805bd87d0e5089a
641i130 <46945263+641i130@users.noreply.github.com>
2023-11-12 14:43:23 -0600

    USER INPUT FUCKING WORKS WTF

--- src/main.c
+++ src/main.c
@@ -1,16 +1,13 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 #include "hardware/gpio.h"
 
 // For memcpy
 #include <string.h>
+
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
 void get_random_data(char *buf, uint16_t len) {
     // Copied from https://github.com/evinrude/pico-rng
@@ -31,39 +28,78 @@ void get_random_data(char *buf, uint16_t len) {
     gpio_put(25, 0);
 }
 
+void init_led() {
+    #ifndef PICO_DEFAULT_LED_PIN
+    #warning Blink requires a board with the default LED
+    #else
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    #endif
+}
+
+void blink(int t) {
+    gpio_put(LED_PIN, 1);
+    sleep_ms(t);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(t);
+}
+
+#define MAX_INPUT_LENGTH 100
+
+char* getInputString() {
+    char *inputString = (char *)malloc(MAX_INPUT_LENGTH * sizeof(char));
+    
+    if (inputString == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Enter a string: ");
+
+    char c;
+    int index = 0;
+
+    while (1) {
+        c = getchar();
+        putchar(c);  // Print the character as it is typed
+        // Store the character in the inputString
+        inputString[index] = c;
+        index++;
+        
+        if (c == '\n' || c == '\r' || c == EOF) {
+            break;  // Exit the loop when Enter is pressed
+        }
+        // Check if the inputString is about to exceed the maximum length
+        if (index >= MAX_INPUT_LENGTH - 1) {
+            fprintf(stderr, "Input length exceeds the maximum limit.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    inputString[index] = '\0';  // Null-terminate the string
+
+    return inputString;
+}
+
 int main() {
-    stdio_init_all();
+    stdio_init_all(); // Setup serial over USB
     adc_init();
     adc_gpio_init(26);
     adc_select_input(0);
-    
+    init_led();
+    sleep_ms(3000);
+    printf("\n\n\n***\n");
+    printf("BOOTED!\n");
     while (true) {
-        #ifndef PICO_DEFAULT_LED_PIN
-        #warning Blink requires a board with the default LED
-        #else
-        const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-        gpio_init(LED_PIN);
-        gpio_set_dir(LED_PIN, GPIO_OUT);
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-        #endif
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-        // Example: Initialize the random data buffer
-        char random_data[10];  // Change the size as needed
-        uint16_t data_length = sizeof(random_data);
+        printf("Hello! What is your name? \n");
+        char *userInput = getInputString();
 
-        // Call the function to get random data
-        get_random_data(random_data, data_length);
+        // You can now use the userInput string as needed
+        printf("\nEntered string: %s\n", userInput);
 
-        // Print the random data using printf
-        printf("Random Data: ");
-        for (int i = 0; i < data_length; ++i) {
-            printf("%02X ", (unsigned char)random_data[i]);
-        }
-        printf("\n");
+        // Don't forget to free the allocated memory
+        free(userInput);
         sleep_ms(1000);
+        blink(10);
     }
 }

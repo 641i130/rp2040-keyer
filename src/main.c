fedf9fcf551e39e5ff8d9ba6eb11c9efaa7b2090
641i130 <46945263+641i130@users.noreply.github.com>
2023-11-18 15:05:40 -0600

    Printing intro with userinput works

--- src/main.c
+++ src/main.c
@@ -10,13 +10,15 @@
 // Global vars and stuff
 #define LED_PIN PICO_DEFAULT_LED_PIN
 #define MAX_INPUT_LENGTH 100
+#define RPIN 25
+#define ADCCH 0
 
 void get_random_data(char *buf, uint16_t len) {
     // Copied from https://github.com/evinrude/pico-rng
     uint16_t adc_result;
     uint8_t size;
     int i;
-    gpio_put(25, 1);
+    gpio_put(RPIN, 1);
     if(len > 64) {
         //TODO handle length error
         size = 64;
@@ -27,7 +29,7 @@ void get_random_data(char *buf, uint16_t len) {
         adc_result = adc_read();
         memcpy(&buf[i-1], (void*)&adc_result, 2);
     }
-    gpio_put(25, 0);
+    gpio_put(RPIN, 0);
 }
 
 void init_led() {
@@ -78,52 +80,61 @@ char* getInputString() {
 void rsleep(uint32_t min_ms, uint32_t max_ms) {
     char buf[64]; // Assuming the buffer size is 64
     uint16_t len = sizeof(buf);
-
     // Get random data
     get_random_data(buf, len);
-
     // Calculate a random sleep duration based on the data
     uint64_t sum = 0;
     for (int i = 0; i < len; i++) {
         sum += buf[i];
     }
-
     // Normalize the sum to be in the range [min_ms, max_ms]
     uint32_t sleep_duration = (sum % (max_ms - min_ms + 1)) + min_ms;
-
     // Sleep for the calculated duration
     sleep_ms(sleep_duration);
-
     // For testing purposes, print the sleep duration
     //printf("Slept for %u milliseconds\n", sleep_duration);
 }
 
+void space() {
+    char buf[64]; // Assuming the buffer size is 64
+    uint16_t len = sizeof(buf);
+
+    for (int j = 0; j < 200; ++j) { // Adjust the number of iterations as needed
+        for (int i = 0; i < 64; ++i) { // Adjust the number of iterations as needed
+            get_random_data(buf, len);
+            char character_to_print = (buf[i] % 4 == 0) ? '*' : ' ';
+            printf("%c", character_to_print);
+        }
+        printf("\n");
+        sleep_ms(10); // 100,000 microseconds = 100 milliseconds
+    }
+}
 
 void intro() {
     // Randomize the intro and make it somewhat faithful to u-boot (to give them familiarity when hacking real devices)
     printf("In:    serial\n");
-    rsleep(100, 200);
+    rsleep(10, 200);
     printf("Out:   serial\n");
-    rsleep(100, 200);
+    rsleep(10, 200);
     printf("Err:   serial\n");
-    rsleep(100, 200);
+    rsleep(10, 200);
     printf("Die ID #2a8800010000000001433a3a0800e01b\n");
-    rsleep(100, 300);
+    rsleep(10, 300);
     printf("4-bit x-loader detected, on-die ecc enabled\n");
-    rsleep(100, 300);
+    rsleep(10, 300);
     printf("g_bootData1.bootableconfig 0x0000008f, lastbootableconfig 0x00000080 ...\n");
-    rsleep(100, 500);
+    rsleep(10, 500);
     printf("g_bootData2.bootableconfig 0x0000008f, lastbootableconfig 0x00000080 ...\n");
-    rsleep(100, 700);
+    rsleep(10, 700);
     printf("eccflags 0x00000000 ecc_status 0x55555555\n");
-    rsleep(100, 200);
+    rsleep(10, 200);
     printf("Current rootmtd: /dev/mtdblock11\n");
-    rsleep(100, 200);
+    rsleep(10, 200);
     printf("Current kerneladdr: 0x700000\n");
-    rsleep(100, 200);
+    rsleep(10, 200);
     printf("\n");
-    rsleep(100, 300);
-    printf("@@@@@@@@@@@@ starting TI u-boot 2  @@@@@@@@@@@@@\n");
+    rsleep(10, 300);
+    printf("@@@@@@@@@@@@ starting RP2040 pi-boot 2  @@@@@@@@@@@@@\n");
     rsleep(10, 100);
     printf("Press space key to stop the auto boot\n");
     printf(": ");
@@ -132,18 +143,18 @@ void intro() {
 int main() {
     stdio_init_all(); // Setup serial over USB
     adc_init();
-    adc_gpio_init(26);
-    adc_select_input(0);
+    adc_gpio_init(RPIN);
+    adc_select_input(ADCCH);
     init_led();
     sleep_ms(3000);
-    printf("\n\n\n***\n");
-    printf("BOOTED!\n");
+    space();
     intro(); // Run text intro
     char *userInput = getInputString();
     // You can now use the userInput string as needed
     printf("\nEntered string: %s\n", userInput);
     // Don't forget to free the allocated memory
     free(userInput);
+
     while (true) {
         sleep_ms(1000);
         blink(10);

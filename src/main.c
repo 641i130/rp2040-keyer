acdc5aa0e30c6220d79dd0ba1b07a657a2527948
641i130 <46945263+641i130@users.noreply.github.com>
2023-11-18 12:18:33 -0600

    Printing intro with userinput works

--- src/main.c
+++ src/main.c
@@ -7,7 +7,9 @@
 // For memcpy
 #include <string.h>
 
-const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+// Global vars and stuff
+#define LED_PIN PICO_DEFAULT_LED_PIN
+#define MAX_INPUT_LENGTH 100
 
 void get_random_data(char *buf, uint16_t len) {
     // Copied from https://github.com/evinrude/pico-rng
@@ -44,28 +46,21 @@ void blink(int t) {
     sleep_ms(t);
 }
 
-#define MAX_INPUT_LENGTH 100
-
+// Read USER INPUT BECAUSE ITS NOT PART OF THE STANDARD LIBRARY FOR SOME REASON
 char* getInputString() {
     char *inputString = (char *)malloc(MAX_INPUT_LENGTH * sizeof(char));
-    
     if (inputString == NULL) {
         fprintf(stderr, "Memory allocation failed.\n");
         exit(EXIT_FAILURE);
     }
-
-    printf("Enter a string: ");
-
     char c;
     int index = 0;
-
     while (1) {
         c = getchar();
         putchar(c);  // Print the character as it is typed
         // Store the character in the inputString
         inputString[index] = c;
         index++;
-        
         if (c == '\n' || c == '\r' || c == EOF) {
             break;  // Exit the loop when Enter is pressed
         }
@@ -75,10 +70,63 @@ char* getInputString() {
             exit(EXIT_FAILURE);
         }
     }
-
     inputString[index] = '\0';  // Null-terminate the string
-
     return inputString;
+}
+
+
+void rsleep(uint32_t min_ms, uint32_t max_ms) {
+    char buf[64]; // Assuming the buffer size is 64
+    uint16_t len = sizeof(buf);
+
+    // Get random data
+    get_random_data(buf, len);
+
+    // Calculate a random sleep duration based on the data
+    uint64_t sum = 0;
+    for (int i = 0; i < len; i++) {
+        sum += buf[i];
+    }
+
+    // Normalize the sum to be in the range [min_ms, max_ms]
+    uint32_t sleep_duration = (sum % (max_ms - min_ms + 1)) + min_ms;
+
+    // Sleep for the calculated duration
+    sleep_ms(sleep_duration);
+
+    // For testing purposes, print the sleep duration
+    //printf("Slept for %u milliseconds\n", sleep_duration);
+}
+
+
+void intro() {
+    // Randomize the intro and make it somewhat faithful to u-boot (to give them familiarity when hacking real devices)
+    printf("In:    serial\n");
+    rsleep(100, 200);
+    printf("Out:   serial\n");
+    rsleep(100, 200);
+    printf("Err:   serial\n");
+    rsleep(100, 200);
+    printf("Die ID #2a8800010000000001433a3a0800e01b\n");
+    rsleep(100, 300);
+    printf("4-bit x-loader detected, on-die ecc enabled\n");
+    rsleep(100, 300);
+    printf("g_bootData1.bootableconfig 0x0000008f, lastbootableconfig 0x00000080 ...\n");
+    rsleep(100, 500);
+    printf("g_bootData2.bootableconfig 0x0000008f, lastbootableconfig 0x00000080 ...\n");
+    rsleep(100, 700);
+    printf("eccflags 0x00000000 ecc_status 0x55555555\n");
+    rsleep(100, 200);
+    printf("Current rootmtd: /dev/mtdblock11\n");
+    rsleep(100, 200);
+    printf("Current kerneladdr: 0x700000\n");
+    rsleep(100, 200);
+    printf("\n");
+    rsleep(100, 300);
+    printf("@@@@@@@@@@@@ starting TI u-boot 2  @@@@@@@@@@@@@\n");
+    rsleep(10, 100);
+    printf("Press space key to stop the auto boot\n");
+    printf(": ");
 }
 
 int main() {
@@ -90,15 +138,13 @@ int main() {
     sleep_ms(3000);
     printf("\n\n\n***\n");
     printf("BOOTED!\n");
+    intro(); // Run text intro
+    char *userInput = getInputString();
+    // You can now use the userInput string as needed
+    printf("\nEntered string: %s\n", userInput);
+    // Don't forget to free the allocated memory
+    free(userInput);
     while (true) {
-        printf("Hello! What is your name? \n");
-        char *userInput = getInputString();
-
-        // You can now use the userInput string as needed
-        printf("\nEntered string: %s\n", userInput);
-
-        // Don't forget to free the allocated memory
-        free(userInput);
         sleep_ms(1000);
         blink(10);
     }

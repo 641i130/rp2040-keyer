375b7abc9c0cef7243f1d12b70d6877399f2c300
641i130 <46945263+641i130@users.noreply.github.com>
2023-11-27 00:07:08 -0600

    Structure

--- src/main.c
+++ src/main.c
@@ -3,11 +3,12 @@
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 #include "hardware/gpio.h"
-
+// Mine
 #include "chisai.h"
 
 int main() {
     stdio_init_all(); // Setup serial over USB
+    printf("DaemonCTF{lift_off}");
     // Set up bootsel button to flash
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
